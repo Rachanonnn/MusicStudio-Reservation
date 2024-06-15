075bc0ca2a7944aee064235c2f03b804edd0e6d5
jowkemm <pimkote1615@gmail.com>
2024-06-16 00:22:15 +0700

    feat: setup mongoose

--- Backend/server.js
+++ Backend/server.js
@@ -1,19 +1,26 @@
 const express = require('express');
+const connectDB = require('./config/db');
+const dotenv = require('dotenv');
+
+//Load env vars
+dotenv.config({path:'./config/config.env' });
+
+//Connect to database
+connectDB();
+
 const app = express();
 
-// Example defining a route in Express
+const PORT = process.env.PORT || 3000;
+const server = app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode on ${process.env.HOST}: ${PORT}`));
+
+
 app.get('/', (req, res) => {
     res.send('<h1>Hello, Express.js Server!</h1>');
 });
 
-// Include route files
-const userRoute = require('./routes/users');
-
-// Use routes
-app.use("/users", userRoute);
-
-const port = process.env.PORT || 3000;
-
-server.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+//Handle unhandled promise rejections
+process.on('unhandledRejection',(err,promise)=>{
+    console.log(`Error: ${err.message}`);
+    //Close server & exit process
+    server.close(()=>process.exit(1));
+})
